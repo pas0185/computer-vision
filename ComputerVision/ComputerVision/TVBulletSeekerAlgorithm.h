2949de5b7e2795607287e2749b7ff98a71fd3dc9
Patrick Sheehan <psheehan0301@gmail.com>
2015-06-02 16:06:42 -0500

    Density calculation function in BulletSeekerAlg

--- ComputerVision/ComputerVision/TVBulletSeekerAlgorithm.h
+++ ComputerVision/ComputerVision/TVBulletSeekerAlgorithm.h
@@ -8,12 +8,17 @@
 
 #import "Headers.h"
 
+// Value between [0-1], used in density filter
+#define BULLET_DENSITY_THRESHOLD 0.75
+
 @interface TVBulletSeekerAlgorithm : NSObject
 
 // NOTE: '+' means static/class method (can be called without creating an instance of the class
 // *delete this after you read it*
 
 + (NSArray *)getTVBulletCandidatesFromDiffMatrix:(cv::Mat)diff;
+
++ (NSArray *)filterTVBulletsByDensity:(NSArray *)tvBullets;
 
 
 @end
--- ComputerVision/ComputerVision/TVBulletSeekerAlgorithm.mm
+++ ComputerVision/ComputerVision/TVBulletSeekerAlgorithm.mm
@@ -21,4 +21,55 @@
     return nil;
 }
 
++ (NSArray *)filterTVBulletsByDensity:(NSArray *)tvBullets {
+    /*
+     Given an array of TVBullets, return only those which exceed the threshold density
+     */
+    
+    // Extrema variables to track min/max of each bullet
+    int xMin, xMax, yMin, yMax;
+    
+    NSMutableArray *arrDenseBullets = [NSMutableArray new];
+    
+    
+    // Find the min/max X and Y value for the each bullet
+    // Add the ones that are dense enough to the return array
+    for (TVBullet *bullet in tvBullets) {
+        
+        // Set all extrema variables to opposite extreme
+        xMin = INT_MAX, xMax = INT_MIN, yMin = INT_MAX, yMax = INT_MIN;
+        
+        for (NSValue *pixelValue in bullet.pixelArray) {
+            
+            CGPoint pt = [pixelValue CGPointValue];
+            
+            xMin = (pt.x < xMin) ? pt.x : xMin;
+            xMax = (pt.x > xMax) ? pt.x : xMax;
+            
+            yMin = (pt.y < yMin) ? pt.y : yMin;
+            yMax = (pt.y > yMax) ? pt.y : yMax;
+            
+        }
+        
+        // At this point we have the min/max X/Y of the bullet
+        
+        // Calculate the encapsulating border area and actual area (in pixels)
+        CGFloat borderArea = (xMax - xMin) * (yMax - yMin);
+        CGFloat actualArea = bullet.pixelArray.count;
+        CGFloat density = actualArea / borderArea;
+        
+        
+        if (density > BULLET_DENSITY_THRESHOLD) {
+            
+            NSLog(@"Success! Bullet has valid density. (D = %f)", density);
+            [arrDenseBullets addObject:bullet];
+        }
+        else {
+            NSLog(@"Failure! Bullet not dense enough. (D = %f)", density);
+        }
+    }
+    
+    return arrDenseBullets;
+}
+
 @end
